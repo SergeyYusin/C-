9f8ae576896de9bf1ec9863c86bc303f21b5d493
unknown <cruspe123@mail.ru>
2023-05-28 17:59:11 +0300

    2 задача решина

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,13 +1,14 @@
-﻿Console.WriteLine("Введите первое число");
-int numberFirst = Convert.ToInt32(Console.Readline());
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int numberSecond = Convert.ToInt32(Console.Readline());
+int numberSecond = Convert.ToInt32(Console.ReadLine());
 
-if (numberFirst == numberSecond * numberSecond)
+if (numberFirst > numberSecond)
 {
-	Console.WriteLine($"{numberFirst} является квадратом {numberSecond}");
+	Console.WriteLine($"{numberFirst} является больше чем {numberSecond}");
 }
 else
 {
-	Console.WriteLine($"{numberFirst} не квадрат {numberSecond}");
+	Console.WriteLine($"{numberFirst} является меньше чем {numberSecond}");
 }
