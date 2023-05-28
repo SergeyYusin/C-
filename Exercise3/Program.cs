0cd772ff81470ce4846ce1a979cada9eb2e23149
unknown <cruspe123@mail.ru>
2023-05-28 18:40:57 +0300

    4 задача решина

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-    }
-    
-}
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (numberFirst > numberSecond)Console.WriteLine($"> {numberFirst}");
+else if (number > numberSecond)Console.WriteLine($"> {number}");
+else  Console.WriteLine($"> {numberSecond}");
+
