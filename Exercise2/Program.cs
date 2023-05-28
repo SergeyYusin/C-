abb26629abaa2439c8d67a20de933b2dad1b3287
unknown <cruspe123@mail.ru>
2023-05-28 17:46:06 +0300

    qwe

---
+++ Exercise2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число");
+int numberFirst = Convert.ToInt32(Console.Readline());
+Console.WriteLine("Введите второе число");
+int numberSecond = Convert.ToInt32(Console.Readline());
+
+if (numberFirst == numberSecond * numberSecond)
+{
+	Console.WriteLine($"{numberFirst} является квадратом {numberSecond}");
+}
+else
+{
+	Console.WriteLine($"{numberFirst} не квадрат {numberSecond}");
+}
